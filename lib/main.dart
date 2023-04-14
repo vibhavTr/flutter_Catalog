b347783348847bd57e390871a9381b437086f7f2
Vibhav Trivedi <vibhavtrivedi15@gmail.com>
2023-04-14 16:20:24 +0530

    Login page styling & Image adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +14,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       routes: {
-        "/": (context) => const HomePage(),
-        "/home": (context) => const LoginPage(),
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
         "/login": (context) => const LoginPage(),
       },
     );
