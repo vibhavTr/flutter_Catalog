dc9860cee1111df65c3aeae21d9fe7e6da145f8e
Vibhav Trivedi <vibhavtrivedi15@gmail.com>
2023-04-14 15:29:46 +0530

    Pages & Routes Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => const HomePage(),
+        "/home": (context) => const LoginPage(),
+        "/login": (context) => const LoginPage(),
+      },
     );
   }
 }
